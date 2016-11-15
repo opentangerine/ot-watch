c9f84ff0db837d6260eabe54a8d5c3f15f12f8df
Grzegorz Gajos <grzegorz.gajos@opentangerine.com>
2016-11-15 18:36:55 +0100

    #7: snapshot

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 if [ -z "$SONATYPE_USERNAME" ]
 then
-    echo "error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
+    echo "Error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
     exit 1
 fi
 
 if [ -z "$SONATYPE_PASSWORD" ]
 then
-    echo "error: please set SONATYPE_PASSWORD environment variable"
+    echo "Error: please set SONATYPE_PASSWORD environment variable"
     exit 1
 fi
 
 if [ ! -z "$TRAVIS_TAG" ]
 then
-    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+    echo "Tagged build. Overriding project version = $TRAVIS_TAG"
     mvn org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
 else
-    echo "not on a tag -> keep snapshot version in pom.xml"
+    echo "Not on a tag. Publishing snapshot version."
 fi
 
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $TRAVIS_DIR/pubring.gpg.enc -out $TRAVIS_DIR/pubring.gpg -d
